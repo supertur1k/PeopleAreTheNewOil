c2a94b829d2746d4afadd5eec6954878fe97fb29
Grom03 <90113648+Grom03@users.noreply.github.com>
2022-04-28 13:24:05 +0300

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('accounts/login/', views.LoginView.as_view(), name="login"),
+    path('accounts/profile/', views.ProfilePage.as_view(), name="profile"),
+    path(r'^accounts/register/$', views.RegisterView.as_view(), name="register"),
+    path(r'^accounts/register_pro/$', views.RegisterView.as_view(), name="register_pro"),
+    path('', views.home),
     path('login', views.login),
     path('master', views.master),
     path('slave', views.slave)
