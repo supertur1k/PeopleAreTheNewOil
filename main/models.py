68d9fef9e4b9b6909f67752241fc936b57c72921
Mikhail Sazonov <mikhail.sazonov42@gmail.com>
2022-05-03 18:09:01 +0300

    add model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Questions(models.Model):
+    id = models.CharField(max_length=200, primary_key=True)
+    factor = models.CharField(max_length=200)
+    value = models.CharField(max_length=200)
+    date = models.DateTimeField('date published')
