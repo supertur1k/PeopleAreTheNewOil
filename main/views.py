c15ea3966e217fd97f7bde32aade2ce5ea87de94
supertur1k <arturzamilov2001@gmail.com>
2022-05-13 16:45:06 +0300

    MyView в views.py при post запросе теперь рассчитывает результат сотрулника

--- main/views.py
+++ main/views.py
@@ -1,18 +1,10 @@
-from typing import Any
-
-from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout as django_logout
 from django.views.generic import TemplateView
-from django.views import View
-from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-from django.views.decorators.csrf import csrf_protect
 from datetime import datetime
-
-from main.models import Questions, RawQuestions
 
 import sqlite3
 
@@ -27,7 +19,56 @@ class MyView(TemplateView):
         return render(request, 'slave.html')
 
     def post(self, request):
-        print(request.body)
+        arr = request.POST.get('19')
+        username = request.POST.get('username')
+
+        completely_wrong = 0
+        probably_wrong = 0
+        can_not_say = 0
+        probably_correct = 0
+        absolutely_correct = 0
+
+        for i in range(19):
+            if arr[i] == "0":
+                completely_wrong = completely_wrong + 1
+            if arr[i] == "1":
+                probably_wrong = probably_wrong + 1
+            if arr[i] == "2":
+                can_not_say = can_not_say + 1
+            if arr[i] == "3":
+                probably_correct = probably_correct + 1
+            if arr[i] == "4":
+                absolutely_correct = absolutely_correct + 1
+
+        result = ""
+
+        if completely_wrong > 14 or probably_wrong > 18:
+            result = "Сотрудник абсолютно здоров психолигчески и готов к продуктивной работе!"
+
+        elif absolutely_correct > 14 or probably_correct > 18:
+            result = "Сотрудник сталкивается с большими трудностями в работе. Система выявида у сотрудника " \
+                     "признаки эмоцианального выгорания. Настоятельно рекомендуем работодателю провести беседу. " \
+                     "Возможные сценарии: смена коллектива, отпуск, изменение условий работы. "
+
+        elif can_not_say > 14:
+            result = "Система не может однозначно выявить симптомы эмоционального выгорания у сотрудника. " \
+                     "Рекомендуется консультация специалиста"
+
+        elif 14 > completely_wrong > 8 or 18 > probably_wrong > 12:
+            result = "Сотрудник достаточно хорошо справляется с работой, но к увеличению нагрузок с сохранением " \
+                     "текущих условий труда не готов."
+
+        elif 14 > absolutely_correct > 8 or 18 > probably_correct > 12:
+            result = "Ярко выраженных симптомов эмоционального выгорания не выявлено, однако, работодателю " \
+                     "рекомендуется обратить внимания на условия труда своего сотрудника. В случае сохранения " \
+                     "текущих условий без предоставления отпуска, в ближайшем будущем есть риск просадки в" \
+                     " продуктивности."
+
+        else:
+            result = "Результаты опроса не позволяют системе однозначно понять психологическое состояние сотрудника. " \
+                     "Рекомендуется консультация со специалистом."
+
+        print("Сотрудник: ", username, "\nРезультат: ", result)
         return render(request, 'home.html')
 
 
@@ -42,7 +83,6 @@ def master(request):
 def slave(request):
     if request.method == 'POST':
         print('Goliath online')
-
 
 
 def results(request):
